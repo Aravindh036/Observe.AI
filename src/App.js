abddae1ca3b032f4ccb43c2eb661f613cecc46c3
Aravindh036 <aravindh036@gmail.com>
2018-12-09 17:45:45 +0530

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import searchIcon from './assets/search.png';
+import user from './assets/user.png';
 
+import PostItem from './components/PostItem/PostItem';
 class App extends Component {
+  state={
+    posts:[]
+  }
+  componentWillMount(){
+    fetch("http://starlord.hackerearth.com/insta")
+    .then(res=>res.json())
+    .then(res=>{
+      console.log(res);
+      this.setState({
+        posts:res
+      });
+    })
+    
+  }
   render() {
+    let i=0;
+    let posts = this.state.posts.map(post=>{
+      i+=1;
+      return <PostItem url = {post.Image} like = {post.likes} time = {post.timestamp} key={i}
+      />
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+       <nav className="app-nav">
+         <div className="div-inner">
+           <span>Instagram</span>
+           <div className="search">
+              <input className="search-input" type="text" placeholder="search"/>
+              <img className="search-icon"  src={searchIcon} alt=""/>
+           </div>
+         </div>
+       </nav>
+      <div className="post-container">
+        <div className="posts">
+            <div className="user-post">
+              <div className="user-icon">
+                <img src={user} alt=""/>
+                <span>Observe.AI</span>
+              </div>
+              <button>Add Story</button>
+            </div>
+            {posts}
+        </div>
+      </div>
       </div>
     );
   }
---
+++ src/components/PostItem/PostItem.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+
+import './PostItem.css';
+export default class PostItem extends Component {
+  state={
+    likes:this.props.like,
+    comments:[]
+  }
+  likeAction=(e)=>{
+    if(!e.target.classList.contains("liked")){
+      e.target.classList.add("liked");
+    }
+    this.setState({
+      likes:this.state.likes+1
+    });
+  }
+  showComments=(e)=>{
+    var node = e.target.nextSibling;
+    node.classList.toggle("hide");
+  }
+  keyup=(e)=>{
+    var key = e.which || e.keyCode;
+      if (key === 13) { // 13 is enter
+        console.log(e.target.value);
+        let obj={
+          content:e.target.value
+        }
+        let newobj = [];
+        newobj = this.state.comments;
+        newobj.push(obj);
+        this.setState({
+          comments:newobj
+        })
+        e.target.value = "";
+      }
+  }
+  render() {
+    sessionStorage.setItem('likes',this.state.likes)
+    let comments;
+    if(this.state.comments.length==0){
+      let defaultComment = [1];
+      comments = defaultComment.map(cmt=>{
+        return <p className="default-comment">no comments</p>
+      })
+    }
+    else{
+      comments = this.state.comments.map(cmt=>{
+        return <p className="comment-items">{cmt.content}</p>
+      })
+    }
+    console.log("commment",comments);
+    return (
+      <div className="post-item--container">
+        <div className="image-holder">
+            <img src={this.props.url} alt="" />
+        </div>
+        <div className="like-comment">
+            <div>
+              <div class="heart" onClick={this.likeAction}></div>
+              <div className="like-count">{sessionStorage.getItem("likes")}</div>
+            </div>
+            <div>
+              <div className="show-comments" onClick={this.showComments}>Show comments</div>
+              <div className="comment-section hide" id="comment-section-id">
+                {comments}
+              </div>
+            </div>
+        </div>
+        <input className="comment" id="comment-input-id" type="text" placeholder="Add a comment..." onKeyUp={this.keyup}/>
+      </div>
+    )
+  }
+}
